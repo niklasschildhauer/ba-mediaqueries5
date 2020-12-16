175ec272012fff32a5a5893c35e683771a1cb294
Niklas Schildhauer <ns107@hdm-stuttgart.de>
2020-12-16 10:40:15 +0100

    BA-14 - comment model.ts

--- src/model/Model.ts
+++ src/model/Model.ts
@@ -8,6 +8,14 @@ export interface IMediaDescriptor {
     body: string;
 }
 
+/**
+ * @interface IMediaQuery
+ *
+ * Represents a CSS media query. It divides the query into browser
+ * supported and unsupported ones. The unsupported ones are
+ * those which contains common term media features. A media query
+ * can be negated, which means that 'not' is in front of the query.
+ */
 export interface IMediaQuery {
     unSupportedMediaQuery: IMediaFeature[];
     supportedMediaQuery: string | null;
@@ -15,21 +23,45 @@ export interface IMediaQuery {
 
 }
 
+/**
+ * @interface IMediaFeature
+ *
+ * Represents a CSS media feature. In this case the media feature is a
+ * common term media feature, which belongs to the unsupported media query.
+ * A media query consists of a media type and 0 <= media features.
+ * A media feature can be negated, which means that 'not' is in front of the feature.
+ */
 export interface IMediaFeature {
     mediaFeature: CommonTerm;
     value: string;
     negated: boolean;
 }
 
-
+/**
+ * @interface IUserPreference
+ *
+ * The user context consists of various user preferences. In this case they representing
+ * a Common Term media feature and the associated value.
+ */
 export interface IUserPreference {
     mediaFeature: CommonTerm;
     value: string;
 
 }
 
-
+/**
+ * @class CommonTermUtil
+ *
+ * Helper Class with static functions
+ */
 export class CommonTermUtil {
+    /**
+     * Static function to check if a query contains a common term media feature
+     *
+     * @param query  A string of a media query.
+     * @returns A tuple with a boolean if the query contains a common term media feature
+     * and the media feature if it does.
+     */
     public static  containsCommonTermMediaFeature(query: string): [boolean, CommonTerm | null] {
         let result: [boolean, CommonTerm | null] = [false, null];
         Object.keys(CommonTerm).forEach((key) => {
@@ -42,6 +74,9 @@ export class CommonTermUtil {
     }
 }
 
+/**
+ * @class MediaQuery
+ */
 export class MediaQuery implements IMediaQuery {
     negated: boolean;
     unSupportedMediaQuery: IMediaFeature[];
@@ -56,6 +91,9 @@ export class MediaQuery implements IMediaQuery {
 
 }
 
+/**
+ * @class MediaDescriptor
+ */
 export class MediaDescriptor implements IMediaDescriptor {
     mediaQuery: IMediaQuery;
     body: string;
@@ -66,6 +104,13 @@ export class MediaDescriptor implements IMediaDescriptor {
     }
 }
 
+/**
+ * @interface ICommonTermList
+ *
+ * Is the implementation of the MediaList for the Common Term Media features. Like the MediaList,
+ * the CommonTermList should also be able to be queried in JS in order to be able to use
+ * media queries there. An EventListener can be stored to be notified when the value of the media query changes.
+ */
 export interface ICommonTermList {
     callbackFunction: () => any;
     mediaQuery: IMediaQuery;
@@ -74,40 +119,48 @@ export interface ICommonTermList {
     setMatchValue(value: boolean): void;
 }
 
+/**
+ * @class CommonTermList
+ */
 export class CommonTermList implements ICommonTermList {
     mediaQuery: IMediaQuery;
     private matchValue: boolean;
     callbackFunction: () => any = () => void 0;
-
-    addListener(event: string, callback: () => any): void {
-        this.callbackFunction = callback;
-    }
-
-    matches():boolean {
-        return this.matchValue;
-    }
 
     constructor(unSupportedMediaQuery: IMediaFeature[], supportedMediaQuery: string | null, negated: boolean) {
         this.mediaQuery = new MediaQuery(unSupportedMediaQuery, supportedMediaQuery, negated);
         this.matchValue = false
     }
 
+    /**
+     * Adds a Event Listener, which should be called, when the value changes
+     *
+     * @param event   For example "change"
+     * @param callback   The function which should be executet.
+     */
+    addListener(event: string, callback: () => any): void {
+        this.callbackFunction = callback;
+    }
+
+    /**
+     * @returns True if the media query does match
+     */
+    matches():boolean {
+        return this.matchValue;
+    }
+
+    /**
+     * @param value   Sets the match value.
+     */
     setMatchValue(value: boolean): void {
         this.matchValue = value;
     }
 
 }
 
-export interface IVideoElement {
-
-}
-
-export class VideoElement implements IVideoElement {
-
-}
-
-
-
+/**
+ * @class MediaFeature
+ */
 export class MediaFeature implements IMediaFeature {
     mediaFeature: CommonTerm;
     negated: boolean;
@@ -120,6 +173,9 @@ export class MediaFeature implements IMediaFeature {
     }
 }
 
+/**
+ * @class UserPreference
+ */
 export class UserPreference implements IUserPreference {
     mediaFeature: CommonTerm;
     value: string;
@@ -131,6 +187,11 @@ export class UserPreference implements IUserPreference {
 
 }
 
+/**
+ * @enum CommonTerm
+ *
+ * Contains all new media features from the common terms.
+ */
 export enum CommonTerm {
     displaySkiplinks = "displaySkiplinks",
     audioDescriptionEnabled = "audioDescriptionEnabled",
@@ -144,6 +205,12 @@ export enum CommonTerm {
     sessionTimeout = "sessionTimeout"
 }
 
+/**
+ * @enum Persona
+ *
+ * Contains all Personas from the 'A day in the life of …' descriptions
+ * [Markdown](https://moocap.gpii.eu/?page_id=33).
+ */
 export enum Persona {
     alexander = "Alexander",
     anna = "Anna",
@@ -156,6 +223,12 @@ export enum Persona {
     tom = "Tom"
 }
 
+/**
+ * @enum SkipLinkValues
+ *
+ * The general term media feature 'dispalySkiplinks' has three values to choose from.
+ * It is the only one one that is not boolean and has a value of a set.
+ */
 export enum SkipLinkValues {
     onfocus = "onfocus",
     always = "always",
